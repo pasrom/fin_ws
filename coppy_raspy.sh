01c62429c05fb1e9216ffed0314e0d5c8837cbf8
pasrom <32713472+pasrom@users.noreply.github.com>
2017-12-29 22:22:43 +0100

    delete test line

--- coppy_raspy.sh
+++ coppy_raspy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#scp -r ~/gopigo_ws roman@192.168.0.105:/home/roman/gopigo_ws
 rsync -avr -e "ssh -l roman" --exclude 'build' --exclude 'devel' ~/gopigo_ws* 192.168.0.105:/home/roman/
